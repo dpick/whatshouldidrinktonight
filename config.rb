8d268b3e229bb7be8588b794d69951493709165c
David Pick <pickdavid@gmail.com>
2010-10-08 14:33:57 -0400

    still trying to make dm work with heroku

--- config.rb
+++ config.rb
@@ -1,8 +1,8 @@
+require 'rubygems'
 require 'dm-core'
 require 'dm-migrations'
 
 configure :development do
-  #DataMapper.setup(:default, 'sqlite://' + Dir.pwd + '/development.db')
   DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://development.db') 
 end
 
